ab4e7b3f5a60690d932ee606142192729f51e40f
Obi Oberoi <obi@eOberoi.com>
2021-01-06 12:47:27 -0500

    added EF packages and DbContext

---
+++ EFCCoreNewFeatues/Repositories/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCCoreNewFeatues.Repositories
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
