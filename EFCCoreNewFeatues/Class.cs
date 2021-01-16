3ffb23535675e640252f6eb5b280ddc1b752017c
Obi Oberoi <obi@eOberoi.com>
2021-01-16 13:53:54 -0500

    added a new class Class

---
+++ EFCCoreNewFeatues/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCCoreNewFeatues
+{
+    public class Class
+    {
+        public int ClassID { get; set; }
+        public string Title{ get; set; }
+        public string Description { get; set; }
+    }
+}
