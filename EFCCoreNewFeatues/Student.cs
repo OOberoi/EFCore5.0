89afb6a0000c161b4976a7a2fa0a0191d832d056
Obi Oberoi <obi@eOberoi.com>
2021-01-16 13:51:38 -0500

    renamed ManyToMany to Student

---
+++ EFCCoreNewFeatues/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCCoreNewFeatues
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
