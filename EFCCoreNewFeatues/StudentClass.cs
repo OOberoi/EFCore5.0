cd9067cb413bd3fac31e48b04ca8158ede0c0565
Obi Oberoi <obi@eOberoi.com>
2021-01-17 22:00:24 -0500

    added getters/setters to StudentClass

--- EFCCoreNewFeatues/StudentClass.cs
+++ EFCCoreNewFeatues/StudentClass.cs
@@ -7,5 +7,10 @@ namespace EFCCoreNewFeatues
 {
     public class StudentClass
     {
+        public int StudentID { get; set; }
+        public Student Student { get; set; }
+
+        public int ClassID { get; set; }
+        public Class Class { get; set; }
     }
 }
