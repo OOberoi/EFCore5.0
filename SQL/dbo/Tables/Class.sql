0f826508d88397840b3cb486c010b58a042c3fe7
Obi Oberoi <obi@eOberoi.com>
2021-01-17 21:44:07 -0500

    added db schema

---
+++ SQL/dbo/Tables/Class.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Class] (
+    [ClassID]     INT        NOT NULL,
+    [Title]       NCHAR (10) NULL,
+    [Description] NCHAR (25) NULL,
+    CONSTRAINT [PK_Class] PRIMARY KEY CLUSTERED ([ClassID] ASC)
+);
+
---
+++ SQL/dbo/Tables/Student.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [StudentID] INT        NOT NULL,
+    [FirstName] NCHAR (10) NULL,
+    [LastName]  NCHAR (10) NULL,
+    CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ([StudentID] ASC)
+);
+
