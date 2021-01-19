920ab77a095cac40d03b3b25595ca104b9d0ce2e
Obi Oberoi <obi@eOberoi.com>
2021-01-18 22:16:50 -0500

    added GetStudents method

--- EFCCoreNewFeatues/Controllers/StudentsController.cs
+++ EFCCoreNewFeatues/Controllers/StudentsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EFCCoreNewFeatues.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,9 +23,9 @@ namespace EFCCoreNewFeatues.Controllers
 
         // GET: api/<StudentsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
         {
-            return new string[] { "value1", "value2" };
+            return await _context.Student.ToListAsync();
         }
 
         // GET api/<StudentsController>/5
