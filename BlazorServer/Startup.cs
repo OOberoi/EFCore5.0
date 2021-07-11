d95efc6a11f15207668fe44dc1e4223a3ec79988
Obi Oberoi <obi@eoberoi.com>
2021-07-11 07:17:28 -0400

    added minor changes

--- BlazorServer/Startup.cs
+++ BlazorServer/Startup.cs
@@ -28,7 +28,7 @@ namespace BlazorServer
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
